4aa9273c5f73413eb254adbfa1db310376aebd2f
funkyd3v <sdiu.cse215@gmail.com>
2023-12-29 22:15:38 +0600

    event management REST api app

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use tidy;
+
+class EventController extends Controller
+{
+    use CanLoadRelationships;
+    private array $relations = ['user', 'attendees', 'attendees.user'];
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except('index', 'show');
+        $this->authorizeResource(Event::class, 'event');
+    }
+
+    public function index()
+    {
+        
+        $query = $this->loadRelationships(Event::query());
+
+        return EventResource::collection($query->latest()->paginate());
+    }
+
+    public function store(Request $request)
+    {
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time'
+            ]),
+            'user_id' => $request->user()->id
+        ]);
+
+        return new EventResource(
+            $this->loadRelationships($event)
+        );
+    }
+
+    public function show(Event $event)
+    {
+        // $event->load('user', 'attendees');
+        return new EventResource(
+            $this->loadRelationships($event)
+        );
+    }
+
+    public function update(Request $request, Event $event)
+    {
+        // if (Gate::denies('update-event', $event)) {
+        //     abort(403, 'You are not alloewed to update!');
+        // }
+
+        // $this->authorize('update-event', $event);
+
+        $event->update([
+            ...$request->validate([
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'sometimes|date',
+                'end_time' => 'sometimes|date|after:start_time'
+            ])
+        ]);
+
+        return new EventResource(
+            $this->loadRelationships($event)
+        );
+    }
+
+    public function destroy(Event $event)
+    {
+        $event->delete();
+        return response(status: 204);
+    }
+}
---
+++ app/Http/Traits/CanLoadRelationships.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+
+trait CanLoadRelationships
+{
+    public function loadRelationships(Model|EloquentBuilder|QueryBuilder|HasMany $for, ?array $relations = null): Model|EloquentBuilder|QueryBuilder|HasMany
+    {
+        $relations = $relations ?? $this->relations ?? [];
+
+        foreach ($relations as $relation) {
+            $for->when(
+                $this->showIncludeRelation($relation),
+                fn($q) => $for instanceof Model ? $for->load($relation) : $q->with($relation)
+            );
+        }
+        return $for;
+    }
+
+    protected function showIncludeRelation(string $relation): bool
+    {
+        $include = request()->query('include');
+
+        if (!$include) {
+            return false;
+        }
+
+        $relations = array_map('trim', explode(',', $include));
+        
+        return in_array($relation, $relations);
+    }
+
+}
